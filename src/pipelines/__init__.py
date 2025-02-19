cb3a3698e18f157a2222fac42eeaebc0768e758f
yasintuncerr <yasintuncerr@gmail.com>
2025-02-19 15:12:12 +0300

    Add __init__.py to pipelines module for component exports

---
+++ src/pipelines/__init__.py
@@ -0,0 +1,8 @@
+from .elixr_conditioned_img2img import SDElixrConditionedImg2ImgPipeline
+from .vae_processor import VaeProcessor
+from .clip_processor import CLIPTextProcessor
+
+
+
+
+__all__ = [ "SDElixrConditionedImg2ImgPipeline", "VaeProcessor", "CLIPTextProcessor"]
