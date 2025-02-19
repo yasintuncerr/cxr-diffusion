a5a084af031c01f8bf2c1eb449732fc372170491
yasintuncerr <yasintuncerr@gmail.com>
2025-02-20 02:56:53 +0300

    Add ElixrBProjection and FeatureConditionedUNet models to the models module

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,11 @@
-from .projection import ProjectionConfig, BidirectionalProjection
+from .elixrb_projection import ElixrBProjectionConfig, ElixrBProjection,FeatureConditionedUNetConfig,FeatureConditionedUNet
 from .elixrb_classifier import MultiClassifier as ElixrbClassifier
+
+
+__all__ = [
+    'ElixrBProjectionConfig',
+    'ElixrBProjection',
+    'FeatureConditionedUNetConfig',
+    'FeatureConditionedUNet',
+    'ElixrbClassifier'
+]
