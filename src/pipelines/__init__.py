0311860e1435bd8b0285251c566d948927b87df6
yasintuncerr <yasintuncerr@gmail.com>
2025-02-23 16:04:07 +0300

    Add CLIPVisionProcessor and CLIPProcessor to pipeline imports; update __all__ accordingly

--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -1,8 +1,8 @@
 from .elixr_conditioned_img2img import SDElixrConditionedImg2ImgPipeline
 from .vae_processor import VaeProcessor
-from .clip_processor import CLIPTextProcessor
+from .clip_processor import CLIPTextProcessor, CLIPVisionProcessor, CLIPProcessor
 
 
 
 
-__all__ = [ "SDElixrConditionedImg2ImgPipeline", "VaeProcessor", "CLIPTextProcessor"]
+__all__ = [ "SDElixrConditionedImg2ImgPipeline", "VaeProcessor", "CLIPTextProcessor", "CLIPVisionProcessor", "CLIPProcessor" ]
