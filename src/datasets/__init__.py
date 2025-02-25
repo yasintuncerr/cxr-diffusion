9e8c19a00029f08878a304a64336225eb77dca25
yasintuncerr <yasiruncerr@gmail.com>
2025-02-25 21:14:08 +0300

    Remove NIHDataset from __all__ in datasets module

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,4 +1,3 @@
-from .nihcxr14 import NIHDataset
 from .nihcxr14_elixrb_dataset import NIHElixrbDataset
 from .nihcxr14_four_findinglabels_dataset import NIHFourFindingsDataset
 from .nihcxr14_image_dataset import NIHImageDataset
@@ -7,7 +6,6 @@ from .nihcxr14_findinglabels import NIHFindingLabels
 
 
 __all__ = [
-    'NIHDataset',
     'NIHElixrbDataset',
     'NIHFourFindingsDataset',
     'NIHImageDataset',
