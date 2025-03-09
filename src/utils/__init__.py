0af7ab6f38c4eee5b404fa0095cc65b02ca2f026
yasintuncerr <yasintuncerr@gmail.com>
2025-03-09 04:42:57 +0300

    Add feature extractors to utils for enhanced data processing

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,7 @@
 from  .train import Unet2DConditionalTrainer, TrainConfig, get_validation_samples, create_validation_dataloader
 from .helper import decode_str, download, Feature, DsInfo, load_info
+from .feature_extractor import VaeFeatureExtractor, ClipTextFeatureExtractor, ClipVisionFeatureExtractor 
+
 
 __all__ = [
     "Unet2DConditionalTrainer",
@@ -10,5 +12,8 @@ __all__ = [
     "download",
     "Feature",
     "DsInfo",
-    "load_info"
+    "load_info",
+    "VaeFeatureExtractor",
+    "ClipTextFeatureExtractor",
+    "ClipVisionFeatureExtractor"
 ]
