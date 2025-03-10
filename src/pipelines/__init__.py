f78fa376b21baacc594513379c319257badcfe81
yasintuncerr <yasintuncerr@gmail.com>
2025-03-10 04:13:14 +0300

    Rename ClipVisionEmbedder to ClipVisionProcessor for consistency and clarity

--- src/pipelines/__init__.py
+++ src/pipelines/__init__.py
@@ -1,7 +1,7 @@
 from .vae_processor import VaeProcessor
-from .clip_processor import ClipVisionEmbedder, CLIPTextProcessor
+from .clip_processor import ClipVisionProcessor, CLIPTextProcessor
 from .inception_processor import InceptionProcessor
 
 
 
-__all__ = ["VaeProcessor", "CLIPTextProcessor", "ClipVisionEmbedder", "InceptionProcessor"]
+__all__ = ["VaeProcessor", "CLIPTextProcessor", "ClipVisionProcessor", "InceptionProcessor"]
