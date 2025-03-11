32387a2dacfb3e29c53f076fd8a87ed3102997cb
yasintuncerr <yasintuncerr@gmail.com>
2025-03-11 06:09:26 +0300

    Add VAE processing pipeline with encoding and decoding methods

---
+++ src/pipelines/__init__.py
@@ -0,0 +1,7 @@
+from .vae_processor import VaeProcessor
+from .clip_processor import ClipVisionProcessor, CLIPTextProcessor
+from .inception_processor import InceptionProcessor
+
+
+
+__all__ = ["VaeProcessor", "CLIPTextProcessor", "ClipVisionProcessor", "InceptionProcessor"]
