8cc1c64adc70d94edf19e0fcd4a8a0a0d7d9547b
yasintuncerr <yasintuncerr@gmail.com>
2025-03-11 06:04:52 +0300

    Remove unused modules and empty __init__.py files from src and datasets directories

--- src/datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .embed_dataset import EmbeddingDataset
-from .nihcxr_dataset import NIHImageDataset
-from .nihcxr14_elixr_dataset import NIHElixrDataset
-
-__all__ = [
-    "EmbeddingDataset",
-    "NIHImageDataset",
-    "NIHElixrDataset"
-]
